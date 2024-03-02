77243e504ae59ab6d52577f6b48f8d26a4981644
Idris Elgarrab <franckenstein999@gmail.com>
2024-03-02 16:41:00 +0100

    Touch command added

---
+++ touch/touch.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+/**
+ * main - Testing point.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: # 0 (on Success).
+ *         # 1 otherwise.
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	int fd;
+	char *usage = "Usage: ./touch <file_name>...\n";
+
+	if (argc < 2)
+	{
+		printf("%s", usage);
+		return (1);
+	}
+
+	while (*++argv)
+	{
+		if ((fd = open(*argv, O_CREAT, 0600)) == -1)
+			printf("./touch: cannot create file '%s'\n", *argv);
+		else
+			printf("./touch: created file '%s'\n", *argv);
+		close(fd);
+	}
+
+	return (0);
+}
